d39c67053055d11eb827e989fe4bf604ceb93515
Adeyemo Abdullahi <ade72111598@gmail.com>
2025-11-03 16:11:54 -0800

    Initial commit

---
+++ ADE1/server.py
@@ -0,0 +1,30 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+comments = []
+
+@app.route('/')
+def home():
+    return "Server is running and ready to receive comments!"
+
+@app.route('/add_comment', methods=['POST'])
+def add_comment():
+    data = request.get_json()
+    name = data.get('name')
+    comment = data.get('comment')
+    
+    if not name or not comment:
+        return jsonify({'error': 'Name and comment are required!'}), 400
+    
+    comments.append({'name': name, 'comment': comment})
+    return jsonify({'message': 'Comment added successfully!', 'comments': comments})
+
+@app.route('/get_comments', methods=['GET'])
+def get_comments():
+    return jsonify(comments)
+
+if __name__ == '__main__':
+    app.run(debug=True)
